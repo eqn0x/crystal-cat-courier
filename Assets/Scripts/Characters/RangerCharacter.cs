15b3218ba39ef2de289003b5fa65b46d60ffd697
jasmine-green-tea <shelldary@yandex.ru>
2025-07-20 22:20:05 +0300

    fix: warrior to ranger

---
+++ Assets/Scripts/Characters/RangerCharacter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangerCharacter : MonoBehaviour, ICharacter
+{
+    public void UseAttack(bool attackStatus)
+    {
+        if (attackStatus)
+            Debug.Log("Ranger uses attack!");
+        else
+            Debug.Log("Ranger stops using attack!");
+
+        // preform some attack logic for Ranger
+    }
+    public void UseBlock(bool blockStatus)
+    {
+        if (blockStatus)
+            Debug.Log("Ranger uses block!");
+        else
+            Debug.Log("Ranger stops using block!");
+    }
+
+    public void UseDodge()
+    {
+        Debug.Log("Ranger uses dodge!");
+    }
+
+    public void UseSpecial()
+    {
+        Debug.Log("Ranger uses special!");
+    }
+}
